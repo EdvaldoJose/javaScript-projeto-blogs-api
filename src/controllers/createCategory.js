1abc0b0df284628ac170b063b15793322df2a414
EdvaldoJose <edvaldojose003@gmail.com>
2023-02-02 10:50:19 -0300

    Requisitos 08

---
+++ src/controllers/createCategory.js
@@ -0,0 +1,14 @@
+// req-08
+const categorieService = require('../services/categories.service');
+
+const addCategories = async (req, res) => {
+  const { name } = req.body;
+
+  const categorie = await categorieService.addCategories(name);
+
+  if (!name) return res.status(400).json({ message: '"name" is required' });
+
+  return res.status(201).json(categorie);
+};
+
+module.exports = addCategories;
