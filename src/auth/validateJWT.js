b6965b9730d9ccb4bcb642d998295066483173af
EdvaldoJose <edvaldojose003@gmail.com>
2023-02-02 10:47:47 -0300

    Requisitos 06-07

--- src/auth/validateJWT.js
+++ src/auth/validateJWT.js
@@ -1,16 +1,18 @@
+// req-03 - req-05
 const { verifyToken } = require('./JWT');
 
 module.exports = async (req, res, next) => {
-  const token = req.header.authorization;
+  const token = req.headers.authorization;
   if (!token) {
-    return res.status(402).json({ token: 'Token not found' });
+    return res.status(401).json({ message: 'Token not found' });
   }
   try {
     const decoded = verifyToken(token);
+
     if (decoded.isError) return res.status(401).json({ message: 'Expired or invalid token' });
-    req.header.userId = decoded.userId;
+    req.headers.userId = decoded.userId; // req-15
     next();
   } catch (err) {
-    return res.status(401).json({ massage: err.message });
+    return res.status(401).json({ message: err.message });
   }
 };
