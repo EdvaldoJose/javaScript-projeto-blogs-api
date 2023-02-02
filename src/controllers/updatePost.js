f518c40ccd89b424d9f4b882e53f927e548ebb60
EdvaldoJose <edvaldojose003@gmail.com>
2023-02-02 10:57:16 -0300

    Requisitos 15

---
+++ src/controllers/updatePost.js
@@ -0,0 +1,16 @@
+// req-15
+const postService = require('../services/post.service');
+
+const updatePost = async (req, res) => {
+  const { title, content } = req.body;
+  const { userId } = req.headers;
+  const { id } = req.params;
+
+  const result = await postService.updatePost(id, title, content, userId);
+
+  if (result.type) return res.status(result.type).json({ message: result.message });
+
+  return res.status(200).json(result);
+};
+
+module.exports = updatePost;
